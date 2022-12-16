f4aa2a3cee74ca71606a050f44ad757887405cde
archanapatil11 <archanapatil998@gmail.com>
2022-12-16 19:32:15 +0530

    added code for AMZ-001

--- operations.py
+++ operations.py
@@ -1,6 +1,13 @@
 def addition (a,b):
     return a+b
 
+def substraction(a,b):
+    return a-b
+
+
 a=50
 b=30
+
 print(f"addition of {a} and {b} is {addition (a,b)}")
+
+print(f"substraction of {a} and {b} is {substraction (a,b)}")
